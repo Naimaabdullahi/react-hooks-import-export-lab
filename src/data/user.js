96fd70fbfae53f0ef556c51265d01f271df916ba
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-04-19 03:54:07 +0300

    finished

--- src/data/user.js
+++ src/data/user.js
@@ -3,4 +3,8 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
+export { username};
+export { city};
+export { image};
+
 // export all three variables as named exports
